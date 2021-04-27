986a98e1373a13d5f5300897d2ded2d211860084
CJCU-CC <108b32079@mailst.cjcu.edu.tw>
2021-04-27 11:55:27 +0800

    0427

--- mysite/models.py
+++ mysite/models.py
@@ -11,7 +11,7 @@ class Mood(models.Model):
 class Post(models.Model):
     mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
     nickname = models.CharField(max_length=10, default='不願意透漏身份的人')
-    msgtitle = models.CharField(max_length=20, default='沒有標題')
+    msgtitle = models.CharField(max_length=10, default='沒有標題')
     message = models.TextField(null=False)
     del_pass = models.CharField(max_length=10)
     pub_time = models.DateTimeField(auto_now=True)
