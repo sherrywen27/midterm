944ff34bcab95372e9698224381d99ac5a282da2
CJCU-CC <108b32079@mailst.cjcu.edu.tw>
2021-04-27 11:04:48 +0800

    0427

---
+++ mysite/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from mysite import models
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'message', 'enabled', 'pub_time')
+    ordering = ('-pub_time',)
+
+
+admin.site.register(models.Mood)
+admin.site.register(models.Post, PostAdmin)
